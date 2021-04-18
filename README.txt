56abeb51c3eeead0e5c42430d304d31fca1e2c07
Ali Razzak <aliarazzak@hotmail.com>
2021-04-18 15:39:23 +1200

    Cleaned up script :japanese_goblin:

--- README.txt
+++ README.txt
@@ -3,11 +3,10 @@ NO COMMERCIAL USE INTENDED
 
 To be run in Python3, Ubuntu Mint Sarah.
 
-This script reads a list of keywords in the "pathogenslist.txt" file (seperated by "\n") and inputs them into the rcsb.org search engine. It then takes the top 25 hits for each keyword and downloads the fasta sequence for each of those proteins. It scans the sequence for englih words and scores them based on sentiment analysis (as scored in the "sentiment-words-DFE-785960.csv") file. Finally it returns a list of (".txt") files in directories associated to each search term with the respective proteins name, sentiment ("+" positive or "-" negative) in column 1, matched word (column 2), and score (column 3). 
+This script reads a list of keywords in the "pathogenslist.txt" file (separated by "\n") and inputs them into the rcsb.org search engine. It then takes the top 25 hits for each keyword and downloads the fasta sequence for each of those proteins. It scans the sequence for english words and scores them based on sentiment analysis (as scored in the "sentiment-words-DFE-785960.csv") file. Finally it returns a list of (".txt") files in directories associated to each search term with the respective proteins name, sentiment ("+" positive or "-" negative) in column 1, matched word (column 2), and score (column 3). 
 
 When using remember to:
 1. have "sentiment-words-DFE-785960.csv", "words.txt", and "pathogenlist.txt" in same directory as "protein_search_word_sentiment.py" and all in working directory.
 2. "pathogenlist.txt" can be edited for other search terms.
 3. path to chrome may be different, if so change "note #1" in script.
-4. make sure variable dlpath pathed to same directory as files download to be default, change at "note #2" in script
-
+4. make sure variable downloaded path to same directory as files download to be default
--- protein_search_word_sentiment.py
+++ protein_search_word_sentiment.py
@@ -19,54 +19,56 @@ import sys
 dlfilepath=str(sys.argv[1])
 scriptpath=str(sys.argv[2])
 
-#Read in sentiment analysis data
+# Read in sentiment analysis data
 sentimentana=pd.read_csv("sentiment-words-DFE-785960.csv")
-positivesentiment=sentimentana[['q1_identify_the_term_that_is_associated_with_the_most_amount_of_positive_sentiment_or_least_amount_of_negative_sentiment','q1_identify_the_term_that_is_associated_with_the_most_amount_of_positive_sentiment_or_least_amount_of_negative_sentiment:confidence']]
-negativesentiment=sentimentana[['q2_identify_the_term_that_is_associated_with_the_most_amount_of_negative_sentiment_or_least_amount_of_positive_sentiment','q2_identify_the_term_that_is_associated_with_the_most_amount_of_negative_sentiment_or_least_amount_of_positive_sentiment:confidence']]
+positivesentiment = sentimentana[['q1_identify_the_term_that_is_associated_with_the_most_amount_of_positive_sentiment_or_least_amount_of_negative_sentiment','q1_identify_the_term_that_is_associated_with_the_most_amount_of_positive_sentiment_or_least_amount_of_negative_sentiment:confidence']]
+negativesentiment = sentimentana[['q2_identify_the_term_that_is_associated_with_the_most_amount_of_negative_sentiment_or_least_amount_of_positive_sentiment','q2_identify_the_term_that_is_associated_with_the_most_amount_of_negative_sentiment_or_least_amount_of_positive_sentiment:confidence']]
 
-#Make list of english words.
+# Make list of english words.
 wordlist=[]
 words = set(x for x in open("words.txt").readlines())
 for i in words:
-    strpi=i.strip()
+    strpi = i.strip()
     if strpi.islower():
         if strpi.isalnum():
-            if len(i)>1:
+            if len(i) > 1:
                 wordlist.append(strpi)
 				
-#Read pathogen list
-withpatho=[]
+# Read pathogen list
+withpatho = []
 with open("pathogenlist.txt") as dis:
     for line in dis:
-        if len(line)>1:
-            wordtrim=line.strip()
-            searchword=wordtrim.replace(" ","%20")
+        if len(line) > 1:
+            wordtrim = line.strip()
+            searchword = wordtrim.replace(" ","%20")
             withpatho.append(searchword)
 
 for qpatho in withpatho:
-    pathogen=qpatho
+    pathogen = qpatho
     path = os.path.join(os.getcwd(), pathogen) 
     os.mkdir(path) 
-    #url path for protein data base search engine
+    #url path for protein data base search engine, dependency on Chrome
     url = 'https://www.rcsb.org/search?request=%7B%22query%22%3A%7B%22parameters%22%3A%7B%22value%22%3A%22'+pathogen+'%22%7D%2C%22type%22%3A%22terminal%22%2C%22service%22%3A%22text%22%2C%22node_id%22%3A0%7D%2C%22return_type%22%3A%22entry%22%2C%22request_options%22%3A%7B%22pager%22%3A%7B%22start%22%3A0%2C%22rows%22%3A100%7D%2C%22scoring_strategy%22%3A%22combined%22%2C%22sort%22%3A%5B%7B%22sort_by%22%3A%22score%22%2C%22direction%22%3A%22desc%22%7D%5D%7D%2C%22request_info%22%3A%7B%22src%22%3A%22ui%22%2C%22query_id%22%3A%224fd19d7ad36839f52b83a7f42c4a6437%22%7D%7D'
-    #note 1, double check chrome exectuable path
 	chrome_path = '/usr/bin/google-chrome %s'
     webbrowser.get(chrome_path).open(url)
+    # Need to slow down scrapping to avoid 403
     time.sleep(5)
     pyautogui.hotkey('ctrl', 's')
     time.sleep(1)
     pyautogui.typewrite(pathogen+"page")
     pyautogui.hotkey('enter')
     time.sleep(8)
-    shutil.move(dlfilepath+"/"+pathogen+"page.html", path+"/"+pathogen+"page.html")
-    f= open(scriptpath+'/'+pathogen+"/"+pathogen+"page.html", "r")
-    f= open(path+"/"+pathogen+"page.html", "r")
+    # Move around directories, need to specific for user
+    shutil.move(dlfilepath + "/" + pathogen + "page.html", path+"/" + pathogen + "page.html")
+    f = open(scriptpath + '/' + pathogen+"/" + pathogen + "page.html", "r")
+    f = open(path + "/" + pathogen + "page.html", "r")
     soup = BeautifulSoup(f, 'lxml')
-    pdblinks=soup.find_all("a", href=True)
-    links=[]
+    pdblinks = soup.find_all("a", href = True)
+    # Collect all PDB hits
+    links = []
     for a in pdblinks:
         links.append(a['href'])
-    onlystructure=[]
+    onlystructure = []
     for l in links:
         if 'structure' in l:
             onlystructure.append(l)
@@ -76,48 +78,48 @@ for qpatho in withpatho:
             if x not in unique_list: 
                 unique_list.append(x)
         return unique_list
-    onlypdb=[]
+    onlypdb = []
     for i in unique(onlystructure):
         onlypdb.append(i[-4:])
     print(onlypdb)
     for i in onlypdb:
-        webbrowser.get(chrome_path).open('https://www.rcsb.org/fasta/entry/'+str(i))
+        webbrowser.get(chrome_path).open('https://www.rcsb.org/fasta/entry/' + str(i))
         time.sleep(2)
-        shutil.move(dlfilepath+"/rcsb_pdb_"+str(i)+".fasta", path+"/rcsb_pdb_"+str(i)+".fasta")
+        shutil.move(dlfilepath + "/rcsb_pdb_" + str(i) + ".fasta", path + "/rcsb_pdb_" + str(i) + ".fasta")
     for i in onlypdb:   
-        pdb=i
-        p= open(path+"/rcsb_pdb_"+str(i)+".fasta", "r")
-        linecount=0
+        pdb = i
+        p = open(path+"/rcsb_pdb_" + str(i) + ".fasta", "r")
+        linecount = 0
         for line in p:
             if ">" not in line:
-                code=line.split('\n')[0]
-        lowcode=(code.lower())
-        pdbcodewords=[]
+                code = line.split('\n')[0]
+        lowcode = (code.lower())
+        pdbcodewords = []
         for i in wordlist:
             if i in lowcode:
                 pdbcodewords.append(i)
-        realwords=[]
+        realwords = []
         d = enchant.Dict("en_US")
         for i in pdbcodewords:
             if d.check(i):
                 realwords.append(i)
-        hitlist=[]
+        hitlist = []
         for i in realwords:
-            sentimentscorepos=positivesentiment[positivesentiment.columns[0]].eq(i)
-            trumatchindex=sentimentscorepos.index[sentimentscorepos==True].tolist()
-            if (len(trumatchindex)>0):
-                thescorespos=positivesentiment.iloc[trumatchindex, 1 ].tolist()
-                hitlist.append("+ "+(i)+" "+str(mean(thescorespos)))
-            sentimentscoreneg=negativesentiment[negativesentiment.columns[0]].eq(i)
-            negmatchindex=sentimentscoreneg.index[sentimentscoreneg==True].tolist()
-            if (len(negmatchindex)>0):
-                thescoresneg=negativesentiment.iloc[negmatchindex, 1 ].tolist()
-                hitlist.append("- "+(i)+" "+str(mean(thescoresneg)))
-        thescorefile=pdb+pathogen+"wordscore.txt"
+            sentimentscorepos = positivesentiment[positivesentiment.columns[0]].eq(i)
+            trumatchindex = sentimentscorepos.index[sentimentscorepos == True].tolist()
+            if (len(trumatchindex) > 0):
+                thescorespos = positivesentiment.iloc[trumatchindex, 1 ].tolist()
+                hitlist.append("+ " + (i) + " " + str(mean(thescorespos)))
+            sentimentscoreneg = negativesentiment[negativesentiment.columns[0]].eq(i)
+            negmatchindex = sentimentscoreneg.index[sentimentscoreneg == True].tolist()
+            if (len(negmatchindex) > 0):
+                thescoresneg = negativesentiment.iloc[negmatchindex, 1 ].tolist()
+                hitlist.append("- " + (i) + " " + str(mean(thescoresneg)))
+        thescorefile = pdb + pathogen + "wordscore.txt"
         with open(thescorefile, "w+") as f:
-            f.write(str(len(hitlist))+"\n")
+            f.write(str(len(hitlist)) + "\n")
             for i in hitlist:
-                f.write(i+"\n")
+                f.write(i + "\n")
         f.close()
         path = os.path.join(os.getcwd(), pathogen) 
-        shutil.move(os.getcwd()+"/"+thescorefile, path+"/"+thescorefile)
+        shutil.move(os.getcwd() + "/" + thescorefile, path + "/" + thescorefile)
